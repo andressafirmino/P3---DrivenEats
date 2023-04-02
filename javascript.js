865acb4582c68c50bb57803b6ccd141bedf77025
andressafirmino <andressafirmino78gmail.comgit remote add origin https://github.com/andressafirmino/P3---DrivenEats.gitclearcd ..lsgit config --global user.email andressafirmino78gmail.com>
2023-04-02 20:48:34 -0300

    Versão com botão finalizar habilitado

--- javascript.js
+++ javascript.js
@@ -1,4 +1,25 @@
+let nome_prato = '';
+let nome_bebida = '';
+let nome_sobremesa = '';
+
+
+function finalizar () {
+    if (nome_prato !== ''){
+        if (nome_bebida !== '') {
+            if (nome_sobremesa !== '') {
+                const continuar = document.querySelector('.botao');
+                continuar.innerHTML = "Fechar pedido";
+                continuar.classList.add('avancar');
+                continuar = document.querySelector('.botao').removeAttribute("disabled");
+            }
+        }
+    }
+
+}
+
 function selecionarPrato (seletor) {
+    nome_prato = seletor.innerHTML;
+
     const selecionadoP = document.querySelector('.prato .selecionado');
 
     if ( selecionadoP !== null ) {
@@ -9,10 +30,15 @@ function selecionarPrato (seletor) {
     
     prato.classList.add('selecionado');
 
+    finalizar();
+
 
 }
 
 function selecionarBebida (seletor) {
+    
+    nome_bebida = seletor.innerHTML;
+
     const selecionadoB = document.querySelector('.bebida .selecionado');
 
     if ( selecionadoB !== null ) {
@@ -23,10 +49,13 @@ function selecionarBebida (seletor) {
     
     bebida.classList.add('selecionado');
 
+    finalizar();
 
 }
 
 function selecionarSobremesa (seletor) {
+    nome_sobremesa = seletor.innerHTML;
+
     const selecionadoS = document.querySelector('.sobremesa .selecionado');
 
     if ( selecionadoS !== null ) {
@@ -37,10 +66,7 @@ function selecionarSobremesa (seletor) {
     
     sobremesa.classList.add('selecionado');
 
-
+    finalizar();
 }
 
-function finalizar () {
-
-}
 
