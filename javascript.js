265a174cc861d47338538c25d22de8884b11e81e
andressafirmino <andressafirmino78gmail.comgit remote add origin https://github.com/andressafirmino/P3---DrivenEats.gitclearcd ..lsgit config --global user.email andressafirmino78gmail.com>
2023-04-03 13:37:56 -0300

    Versão para teste do avaliador

--- javascript.js
+++ javascript.js
@@ -2,23 +2,31 @@ let nome_prato = '';
 let nome_bebida = '';
 let nome_sobremesa = '';
 
+let cliente = '';
+let endereco = '';
 
-function finalizar () {
-    if (nome_prato !== ''){
-        if (nome_bebida !== '') {
-            if (nome_sobremesa !== '') {
-                const continuar = document.querySelector('.botao');
-                continuar.innerHTML = "Fechar pedido";
-                continuar.classList.add('avancar');
-                continuar = document.querySelector('.botao').removeAttribute("disabled");
-            }
-        }
-    }
+let pratoSelecionado = '';
+let bebidaSelecionada = '';
+let sobremesaSelecionada = '';
 
-}
+let valor_prato = '';
+let valor_bebida = '';
+let valor_sobremesa = '';
+
+
+let valor1 ='';
+let valor2 ='';
+let valor3 ='';
+
+let total = 0;
+
+let mensagem = '';
+
+
 
 function selecionarPrato (seletor) {
     nome_prato = seletor.innerHTML;
+    valor_prato = seletor.innerHTML;
 
     const selecionadoP = document.querySelector('.prato .selecionado');
 
@@ -30,6 +38,15 @@ function selecionarPrato (seletor) {
     
     prato.classList.add('selecionado');
 
+    pratoSelecionado = document.querySelector('.prato .selecionado .nome-item');
+    nome_prato = pratoSelecionado.innerHTML;
+
+    valor1 = document.querySelector('.prato .selecionado .preco-item');
+    valor_prato = valor1.innerHTML;
+
+    valor_prato = parseFloat(valor_prato.replace("R$", "").replace(",", "."));
+    console.log(valor_prato);
+
     finalizar();
 
 
@@ -38,6 +55,7 @@ function selecionarPrato (seletor) {
 function selecionarBebida (seletor) {
     
     nome_bebida = seletor.innerHTML;
+    valor_bebida = seletor.innerHTML;
 
     const selecionadoB = document.querySelector('.bebida .selecionado');
 
@@ -49,12 +67,21 @@ function selecionarBebida (seletor) {
     
     bebida.classList.add('selecionado');
 
+    bebidaSelecionada = document.querySelector('.bebida .selecionado .nome-item');
+    nome_bebida = bebidaSelecionada.innerHTML;
+    
+    valor2 = document.querySelector('.bebida .selecionado .preco-item');
+    valor_bebida = valor2.innerHTML;
+
+    valor_bebida = parseFloat(valor_bebida.replace("R$", "").replace(",", "."));
+
     finalizar();
 
 }
 
 function selecionarSobremesa (seletor) {
     nome_sobremesa = seletor.innerHTML;
+    valor_sobremesa = seletor.innerHTML;
 
     const selecionadoS = document.querySelector('.sobremesa .selecionado');
 
@@ -66,7 +93,58 @@ function selecionarSobremesa (seletor) {
     
     sobremesa.classList.add('selecionado');
 
+    sobremesaSelecionada = document.querySelector('.sobremesa .selecionado .nome-item');
+    nome_sobremesa = sobremesaSelecionada.innerHTML;
+    
+    valor3 = document.querySelector('.sobremesa .selecionado .preco-item');
+    valor_sobremesa = valor3.innerHTML;
+
+    valor_sobremesa = parseFloat(valor_sobremesa.replace("R$", "").replace(",", "."));
+
     finalizar();
 }
 
 
+function infosEntrega () {
+    cliente = prompt('Qual o seu nome?');
+    endereco = prompt('Qual o seu endereço?');
+}
+
+
+function finalizar () {
+    if (nome_prato !== ''){
+        if (nome_bebida !== '') {
+            if (nome_sobremesa !== '') {
+                const continuar = document.querySelector('.botao').removeAttribute('disabled');
+                const cont = document.querySelector('.botao');
+                cont.classList.add('avancar');
+                cont.innerHTML = "Fechar pedido";
+            }
+        }
+    }
+
+}
+
+
+function calcular(valor_prato, valor_bebida, valor_sobremesa) {
+   
+    total = (valor_prato) + (valor_bebida) + (valor_sobremesa);
+}
+
+
+function mensagemWpp () {
+   mensagem = "Olá, gostaria de fazer o pedido: " + "\n" +
+                        "- Prato: " + nome_prato + "\n" +
+                        "- Bebida: " + nome_bebida + "\n" +
+                        "- Sobremesa: " + nome_sobremesa + "\n" +
+                        "Total: R$" + total;
+
+     alert(mensagem);                   
+    window.open("https://wa.me/55999999999?text=" + mensagem);
+                        
+}
+
+    
+    
+   
+   
